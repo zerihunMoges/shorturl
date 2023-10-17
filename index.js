3dbdf0a7e4a0f5ac722afcc55ee79ad31e75108a
Zerihun Moges <zerihunmoges78@gmail.com>
2023-10-17 22:59:07 +0300

    add redirects

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const path = require('path')
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yaml'), 'utf-8')
 const redirects = YAML.parse(redirectsFile)
 
+const templateHTML = fs.readFileSync(path.join(__dirname,'template.html'), 'utf-8')
+
+
+for (let [slug, url] of Object.entries(redirects)){
+    const html = templateHTML.replaceAll('https://example.com', url)
+    const folderPath = path.join(__dirname, 'out', slug)
+    fs.mkdirSync(folderPath, {recursive: true})
+    fs.writeFileSync(path.join(folderPath, 'index.html'), html)
+}
